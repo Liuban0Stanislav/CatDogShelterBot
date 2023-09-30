20e5f4692883916ba585b259642ae573d5795c50
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-09-30 18:24:17 +1000

    ветка feature2 создана

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -29,7 +29,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
-
+            // privalov
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }

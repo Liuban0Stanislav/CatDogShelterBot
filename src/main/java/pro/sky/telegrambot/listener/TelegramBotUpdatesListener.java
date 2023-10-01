ad4261fe7f53149ee25f61aa5c8864496063e7e9
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-10-01 18:22:12 +1000

    добавлена команда /start

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,21 +3,30 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.command.Commands;
+import pro.sky.telegrambot.command.StartCommand;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-
     @Autowired
     private TelegramBot telegramBot;
+
+    private final List<Commands> commands;
+
+    public TelegramBotUpdatesListener(List<Commands> commands) {
+        this.commands = commands;
+    }
 
     @PostConstruct
     public void init() {
@@ -28,8 +37,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
-            //feature1
+
+                commands.stream()
+                        .filter(command -> command.ifSuitable(update))
+                        .forEach(command -> command.handle(update));
+
+//                switch (update.message().text()) {
+//                    case "/start":
+//
+//                        break;
+//
+//                }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }

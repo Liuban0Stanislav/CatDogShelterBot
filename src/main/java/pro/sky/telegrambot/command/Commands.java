bd0f7e3beed0c7868801c936e6715f64dcd05689
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-10-07 19:11:31 +1000

    Сдача ДЗ в конце второй недели

--- src/main/java/pro/sky/telegrambot/command/Commands.java
+++
@@ -1,8 +0,0 @@
-package pro.sky.telegrambot.command;
-
-import com.pengrad.telegrambot.model.Update;
-
-public interface Commands {
-    void handle (Update update);
-    boolean ifSuitable (Update update);
-}

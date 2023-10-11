c2115b12250d1fe517e917873d6c23ebaf19756c
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-10-11 17:22:28 +1000

    Добавил меню команд в бота. Теперь команду старт можно найти в меню команд слева от поля ввода сообщений

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,10 +1,15 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static pro.sky.telegrambot.constants.Constants.COMMAND_START;
 
 @Configuration
 public class TelegramBotConfiguration {
@@ -16,6 +21,9 @@ public class TelegramBotConfiguration {
     public TelegramBot telegramBot() {
         TelegramBot bot = new TelegramBot(token);
         bot.execute(new DeleteMyCommands());
+        bot.execute(new SetMyCommands(
+                new BotCommand(COMMAND_START, "Пуск или перезапуск бота")
+        ));
         return bot;
     }
 

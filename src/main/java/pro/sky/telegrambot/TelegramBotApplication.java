9dbf38d18a338f3729077238816e4260faa9c61a
vpm789 <vpm789@gmail.com>
2023-10-13 00:00:09 +0300

    сервисы и контроллеры

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -1,11 +1,13 @@
 package pro.sky.telegrambot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {

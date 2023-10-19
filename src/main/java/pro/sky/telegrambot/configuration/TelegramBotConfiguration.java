39a0802b7ba57cb82548ce012383849203d8d75b
Viacheslav Privalov <privalov2306@yandex.ru>
2023-10-19 11:16:29 +0300

    all works

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!test")
+//@Profile("!test")
 public class TelegramBotConfiguration {
     @Value("${telegram.bot.token}")
     private String token;

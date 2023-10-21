63001a8a8d5431b6d77106d555b9e3d8e5f6ebbd
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-10-21 22:04:06 +1000

    внес правки по замечаниям Алексея

--- src/test/java/pro/sky/telegrambot/TelegramcatdogApplicationTests.java
+++ src/test/java/pro/sky/telegrambot/TelegramcatdogApplicationTests.java
@@ -1,8 +1,11 @@
 package pro.sky.telegrambot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TelegramcatdogApplicationTests {
-
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
 }
--- src/test/java/pro/sky/telegrambot/controller/AdoptionDocControllerTest.java
+++ src/test/java/pro/sky/telegrambot/controller/AdoptionDocControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,7 +21,7 @@ import static pro.sky.telegrambot.constants.DocType.INFO_MEETING_DOG;
 @ActiveProfiles("test")
 class AdoptionDocControllerTest {
 
-    @LocalServerPort
+//    @LocalServerPort
     private int port;
 
     @MockBean
